47e7336e6ad4023833c6ce458e1aaa7a325d7a08
ashutosh <55662919+knerd1@users.noreply.github.com>
2024-04-04 18:47:02 +0530

    Delete basic.js
    deletion

--- basic.js
+++
@@ -1,8 +0,0 @@
-const accountId = 242334
-let accountUserName ="john"
-var accountPassword = "123312"
-accountCity = "Delhi"
-accountPassword= "4342"
-accountUserName= "mcgregor"
-accountCity= "Noida"
-console.log(accountId, accountPassword, accountUserName, accountCity);
