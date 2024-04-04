0690f527139a0855d2607d05cb9a9d06267bc24c
knerd <ap9696446@gmail.com>
2024-04-04 18:28:18 +0530

    coversion

---
+++ 01_basic.js
@@ -0,0 +1,8 @@
+const accountId = 242334
+let accountUserName ="john"
+var accountPassword = "123312"
+accountCity = "Delhi"
+accountPassword= "4342"
+accountUserName= "mcgregor"
+accountCity= "Noida"
+console.log(accountId, accountPassword, accountUserName, accountCity);
---
+++ 03_conversiton_operations.js
@@ -0,0 +1,15 @@
+let score = 22
+let scores = "23, 55"
+console.log(typeof score);
+console.log(typeof scores);
+let abc = Number("scores")
+console.log(abc);
+
+let isLoggedIn = 0
+let booleanIsloggedIn =  Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let oneNumber = 23
+let stringno = String(oneNumber)
+console.log(stringno);
+console.log(typeof(stringno));
