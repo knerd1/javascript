6b817d20408b70eef55d7efc1651dc874e0d2164
knerd <ap9696446@gmail.com>
2024-04-06 19:34:36 +0530

    strings methods

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,30 +1,30 @@
-//  console.log(typeof(null));
-//  console.log(typeof(undefined));
+ console.log(typeof(null));
+ console.log(typeof(undefined));
 
-//  const score = 433
-//  const scoreS = 654.7
-//  const isLogged = true
+ const score = 433
+ const scoreS = 654.7
+ const isLogged = true
 
-//  const outTemp = null
-//  let userEmail;
-//  let Id = Symbol("211")
-//  let secondId = Symbol("211")
-//  console.log(Id===secondId);
+ const outTemp = null
+ let userEmail;
+ let Id = Symbol("211")
+ let secondId = Symbol("211")
+ console.log(Id===secondId);
 
-//  const BigNumber = 2323299n
-//  console.log(typeof(BigNumber));
+ const BigNumber = 2323299n
+ console.log(typeof(BigNumber));
 
-//  let Animes = ["Solo", "AOT", "Tokyo"]
-//  console.log(typeof Animes);
-// let MyObj = {
-//     Name:"Soan",
-//     age: 23,
-// } 
-// console.log(typeof MyObj);
+ let Animes = ["Solo", "AOT", "Tokyo"]
+ console.log(typeof Animes);
+let MyObj = {
+    Name:"Soan",
+    age: 23,
+} 
+console.log(typeof MyObj);
 
-// const myFunction = function(){
-// console.log("hello ji");}
-// console.log(typeof secondId);
+const myFunction = function(){
+console.log("hello ji");}
+console.log(typeof secondId);
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 let myUsername = "ronoroa"
 let NewUsername = myUsername
@@ -32,3 +32,12 @@ let NewUsername = myUsername
 
 console.log(myUsername);
 console.log(NewUsername);
+
+let userOne = {
+    mail: "hello@gmail.com",
+    id: 828
+}
+let userTwo = userOne
+userTwo.mail = "help@gmail.com"
+console.log(userOne);
+console.log(userTwo);
---
+++ 05_strings.js
@@ -0,0 +1,12 @@
+// let name = "anderson"
+// let record = 33-0
+
+// console.log(`Hello my name is ${name} and my one on on record is ${record}`);
+
+const gameName = new String("RedDead")
+console.log(gameName[4]); //--we can acces key value from this
+console.log(gameName.length);
+console.log(gameName.indexOf('a'));
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.toWellFormed(5));
