0085fe689cfbe69ec9cbbc1ea445c9eba88731df
knerd <ap9696446@gmail.com>
2024-04-16 13:06:16 +0530

    arrays

--- 07_datesinjs.js
+++ 07_datesinjs.js
@@ -1,20 +1,25 @@
 let myDate = new Date
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toLocaleTimeString());
-// console.log(myDate.toTimeString());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(typeof myDate);
 
  const newDate = new Date(2024,2,21)
- //  const newDate1 = new Date(2024,1,21,5,6,7)    //with single digit,month will start with 0
-// const newDate2 = new Date("2021-01-23")     //but with yy-dd-mm format month will start with 1
-// console.log(newDate2.toLocaleString());
+  const newDate1 = new Date(2024,1,21,5,6,7)    //with single digit,month will start with 0
+const newDate2 = new Date("2021-01-23")     //but with yy-dd-mm format month will start with 1
+console.log(newDate2.toLocaleString());
 let myTimestamp = Date.now()
 console.log(Math.floor(myTimestamp/1000));
  console.log(newDate.getTime());
  console.log(newDate.getDay());
  console.log(newDate.getMonth()+1);
+
+ myDate.toLocaleString("default",{
+    weekday:"long",
+    hour:"2-digit"
+   })
---
+++ 08_Arrays.js
@@ -0,0 +1,10 @@
+const myArr = [1,3,5,3,2]
+const animes = ['one piece','vinland saga','tokyo revengers']
+const arr2 = new Array(1,3,4,5,45,56)
+console.log(arr2);
+console.log(myArr[2]);
+console.log(animes);
+ // array methods
+ arr2.push(345)   // it will add elements in array\
+ arr2.pop()
+ console.log(arr2);
