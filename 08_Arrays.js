8d80f45a664327c62489ab1ffcd98d7543bbc972
knerd <ap9696446@gmail.com>
2024-04-17 19:35:48 +0530

    arrays

--- 08_Arrays.js
+++ 08_Arrays.js
@@ -1,10 +1,22 @@
-const myArr = [1,3,5,3,2]
-const animes = ['one piece','vinland saga','tokyo revengers']
-const arr2 = new Array(1,3,4,5,45,56)
-console.log(arr2);
-console.log(myArr[2]);
-console.log(animes);
- // array methods
- arr2.push(345)   // it will add elements in array\
- arr2.pop()
- console.log(arr2);
+const myArr = [11,13,52,31,24]
+const animes = ['one piece','vinland saga','tokyo revengers','promiseland','jjk','Attack Titan','Demon Slayer']
+// const arr2 = new Array(1,3,4,5,45,56)
+// // console.log(arr2);
+// // console.log(myArr[2]);
+// // console.log(animes);
+// //  // array methods
+//  arr2.push(345)                 // it will add elements in the end of the array
+//  //  arr2.pop()                 //removes last element form an array
+//  console.log(arr2);
+// // myArr.unshift(533)           //for adding element in start
+// myArr.shift()               //for removing first element in array
+// console.log(myArr);     
+// console.log(myArr.includes(2-1));      //it will give answer in true/false if something exist
+// console.log(myArr.indexOf(3+2));        // it will tell the index value of that element
+
+// const newArry = animes.join()    //add all elements of array into string
+// console.log(newArry);
+// console.log(typeof newArry);
+
+console.log(myArr.slice(1,4));      
+console.log(myArr.splice(0,4));
