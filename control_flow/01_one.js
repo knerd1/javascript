89690e61390c687bc548b7f4d3b566f0def30949
Ashutosh <ap9696446@gmail.com>
2024-07-26 02:49:15 +0530

    logic control

---
+++ control_flow/01_one.js
@@ -0,0 +1,12 @@
+//if
+let isUserLoggedIn = true
+if (isUserLoggedIn<2) {
+     console.log("two = 2");
+}
+if (2>7) {
+     console.log('helllo hunters');
+} else {
+     console.log('losers');
+}
+
+//<,>,<=,=>,==,!=(not)
