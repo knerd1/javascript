6a7050c03fd152d3d7eaa279202f2b4194e0c06a
Ashutosh <ap9696446@gmail.com>
2024-07-29 12:21:05 +0530

    loops

--- iterations/one.js
+++ iterations/one.js
@@ -1,7 +1,16 @@
 for (let i = 0; i <=10; i++) {
      const element = i;
      if (element == 5) {
-          console.log('five hunters');
+         // console.log('five hunters');
      }
-     console.log(element);
+     //console.log(element);
+}
+
+//_______________________________
+for (let i = 0; i <=10; i++) {
+     console.log(`outerloop:${i}`);
+     for (let j = 0; j <=10; j++) {
+    // console.log(`innerloop:${j} and outerloop${i }`);
+    console.log(i + '*' + j + "=" + i*j  );
+     }
 }
